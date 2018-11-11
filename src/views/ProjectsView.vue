df6e545928a18b9972fea470f00e84dce3fed07d
Pablo <psirera4@gmail.com>
2018-11-11 12:42:23 +0100

    Create structure cards on ProjectView

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,17 +1,76 @@
 <template>
   <PageLayout>
-    <h1>This is an project page</h1>
+    <section class="projects">
+      <ProjectCard
+        v-for="(project, index) in projects"
+        :key="index"
+        :project-data="project"
+        class="project"/>
+    </section>
   </PageLayout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import PageLayout from '@/components/PageLayout.vue'
+import ProjectCard from '@/components/ProjectCard.vue'
+import { ProjectCardModel } from '@/models/ProjectCardModel'
 
 export default Vue.extend({
   name: 'ProjectsView',
   components: {
-    PageLayout
-  }
+    PageLayout,
+    ProjectCard
+  },
+  data: (): {
+    projects: ProjectCardModel[]
+  } => ({
+    projects: [
+      {
+        image: 'images/timeline-vuejs.png',
+        name: 'Timeline VueJS',
+        description: 'lorem ipsum aoishduha uwehfewih foehwiofewoh fewocoiwe hiowh ef',
+        links: [
+          {
+            url: 'https://github.com/pablosirera/timeline-vuejs',
+            icon: 'github'
+          },
+          {
+            url: 'https://github.com/pablosirera/timeline-vuejs',
+            icon: 'web'
+          }
+        ]
+      },
+      {
+        image: 'images/my-learning-tracker.png',
+        name: 'My Learning Tracker',
+        description: 'lorem ipsum aoishduha uwehfewih foehwiofewoh fewocoiwe hiowh ef',
+        links: [
+          {
+            url: 'https://github.com/pablosirera/timeline-vuejs',
+            icon: 'github'
+          },
+          {
+            url: 'https://github.com/pablosirera/timeline-vuejs',
+            icon: 'web'
+          }
+        ]
+      }
+    ]
+  })
 })
 </script>
+
+<style lang="scss" scoped>
+.projects {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  & .project {
+    margin-left: 25px;
+    &:first-child {
+      margin-left: 0;
+    }
+  }
+}
+</style>
