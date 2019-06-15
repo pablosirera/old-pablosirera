2694f97e335f6357900df745a87b50814ca0adb8
Pablo Sirera <psirera4@gmail.com>
2019-06-15 21:42:27 +0200

    Fix: links cross origin (#21)
    * fix: links cross-origin add rel nooponer
    
    * feat: update email

--- src/containers/SocialContainer.vue
+++ src/containers/SocialContainer.vue
@@ -3,6 +3,7 @@
     <a 
       v-tooltip.bottom="'twitter.com/pablosirera'"
       target="_blank"
+      rel="noopener noreferrer"
       class="link-icon"
       href="https://twitter.com/pablosirera">
       <Icon name="twitter"/>
@@ -10,6 +11,7 @@
     <a 
       v-tooltip.bottom="'instagram.com/pablodeveloper'"
       target="_blank"
+      rel="noopener noreferrer"
       class="link-icon"
       href="https://www.instagram.com/pablodeveloper">
       <Icon name="instagram"/>
@@ -17,6 +19,7 @@
     <a 
       v-tooltip.bottom="'linkedin.com/in/pablosireramata'"
       target="_blank"
+      rel="noopener noreferrer"
       class="link-icon"
       href="https://www.linkedin.com/in/pablosireramata">
       <Icon name="linkedin"/>
@@ -24,12 +27,13 @@
     <a 
       v-tooltip.bottom="'github.com/pablosirera'"
       target="_blank"
+      rel="noopener noreferrer"
       class="link-icon"
       href="https://github.com/pablosirera">
       <Icon name="github"/>
     </a>
     <a 
-      v-tooltip.bottom="'psirera4@gmail.com'"
+      v-tooltip.bottom="'personal@pablosirera.com'"
       :href="mailtoContent"
       class="link-icon">
       <Icon name="mail"/>
@@ -49,7 +53,7 @@ export default Vue.extend({
   data: (): {
     mailtoContent: string
   } => ({
-    mailtoContent: 'mailto:psirera4@gmail.com?subject=Email%20from%20your%20porfolio'
+    mailtoContent: 'mailto:personal@pablosirera.com?subject=Email%20from%20your%20porfolio'
   })
 })
 </script>
