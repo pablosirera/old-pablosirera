5b0eade42eb2fc0858c4ce072008119b8a992e08
Pablo <psirera4@gmail.com>
2019-08-14 21:36:40 +0200

    feat(blog): order posts for date

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -3,12 +3,12 @@ import { i18n } from '@/langs/i18n'
 
 export const POSTS: IPostsModel[] = [
   {
-    title: i18n.t('posts.deployAppsVue.title', 'es'),
-    date: new Date(2018, 11),
-    nameComponent: 'deploy-apps-vue',
-    id: 0,
-    altImage: i18n.t('posts.addI18nVue.title', 'es'),
-    timeToRead: '1'
+    title: i18n.t('posts.eventBus.title', 'es'),
+    date: new Date(2019, 5),
+    nameComponent: 'event-bus',
+    id: 2,
+    altImage: i18n.t('posts.eventBus.title', 'es'),
+    timeToRead: '2'
   },
   {
     title: i18n.t('posts.addI18nVue.title', 'es'),
@@ -19,12 +19,12 @@ export const POSTS: IPostsModel[] = [
     timeToRead: '4'
   },
   {
-    title: i18n.t('posts.eventBus.title', 'es'),
-    date: new Date(2019, 5),
-    nameComponent: 'event-bus',
-    id: 2,
-    altImage: i18n.t('posts.eventBus.title', 'es'),
-    timeToRead: '2'
+    title: i18n.t('posts.deployAppsVue.title', 'es'),
+    date: new Date(2018, 11),
+    nameComponent: 'deploy-apps-vue',
+    id: 0,
+    altImage: i18n.t('posts.addI18nVue.title', 'es'),
+    timeToRead: '1'
   }
 ]
 
